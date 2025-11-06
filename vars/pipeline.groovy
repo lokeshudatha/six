aa85e5da8dbe13ffa2a6123a477e20876931be6f
lokesh udatha <138110408+lokeshudatha@users.noreply.github.com>
2025-11-06 12:05:49 +0530

    Create pipeline.groovy

---
+++ vars/pipeline.groovy
@@ -0,0 +1,31 @@
+import com.udtha.build.Cal
+
+def call(Map pipelineParams) {
+    Cal cal = new Cal(this)
+
+    pipeline {
+        agent any
+        environment {
+            APP = "${pipelineParams.myapp}"
+        }
+        parameters {
+            string(name: 'user', defaultValue: 'lokesh', description: 'Enter your name here:')
+        }
+        stages {
+            stage('para') {
+                when {
+                    expression {
+                        return params.user == 'lokesh'
+                    }
+                }
+                steps {
+                    sh '''
+                        mkdir -p udatha
+                        cd udatha
+                        echo "success"
+                    '''
+                }
+            }
+        }
+    }
+}
